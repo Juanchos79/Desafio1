5d1d924e0544849f900b1447bb4bb3b39a0c259f
Juan Ares <tecjares@gmail.com>
2022-06-22 17:21:01 -0300

    Primer entrega

---
+++ js/verduleria.js
@@ -0,0 +1,101 @@
+// Entrada de Datos//
+
+let Kilos;
+let precio_producto = 0;
+let precio_total = 0;
+let opcion;
+let FrutaElegida;
+let VerduraElegida;
+    
+    // Procesamiento
+
+function()
+
+do   {
+    opcion = parseInt(prompt("ingrese una opcion: \n 0: terminar compra \n 1: Fruta \n 2: Verdura"));
+    if (opcion == 1){
+        FrutaElegida = prompt("Que fruta desea llevar? opcion: \n 0: terminar compra \n 1: Manzanas \n 2: Bananas \n 3 Tomates"); 
+    
+    }
+else if(opcion == 2) {
+        VerduraElegida = prompt("Que Verdura desea llevar? opcion: \n 0: terminar compra \n 1: Boniatos \n 2: cebolla \n 3 Zapallo");
+    }
+else if(opcion == 0) {
+    alert("Decidiste terminar la compra.");
+    }
+else {
+    alert("INGRESA UNA OPCIÓN VALIDA.");
+}
+    
+
+    
+    
+    
+    
+    /* else if ((opcion) == "" && (opcion) == null) {
+        alert(" No eligio ninguna fruta") 
+    } else (fruta == Manzanas, Bananas, Tomates); {
+        alert));
+        prompt("ingrese:" + (precio_producto) + $);
+        prompt("ingrese:" + (Kilos) + "Kgs");
+        alert(prompt(precio_total = precio_producto * Kilos)); */
+    
+        
+
+    } while (opcion != 0); 
+    
+// el usuario ingresa Si es Fruta o Verdura
+
+// esto no va
+// Elija del Stock de Fruta opciones 0 Volver a preguntar Fruta o Verdura 1 Manzana, 2 Banana, 3 Tomates.
+// Elija del Stock de Fruta opciones stock Verdura Boniatos, Cebolla, Zapallo.
+// esto no va
+
+// Que fruta desea llevar?
+// Manzanas
+// cuantos kilos de (Manzanas)
+// 5
+// el valor de los kilos * precio de Manzanas.
+// Que otraa fruta desea?
+
+// Bananas
+// cuantos kilos de (Manzanas)
+// 5
+// el valor de los kilos * precio de Manzanas.
+
+// muestra lista de Frutas y precios, abajo el total de lo gastado.
+
+// Desea otroa fruta o verdura?
+// opcion 0 terminar programa
+
+
+// luego decide que fruta, luego 
+
+   /* if (opcion == "2") {
+        alert(prompt("Que Verdura desea llevar?:"));
+    } else if (let (opcion) == "" && let (opcion) == null) {
+        alert(" No eligio ninguna Verdura")
+    } else (mercaderia == Boniatos, Cebolla); {
+        alert(prompt(" 1 ¿Cuantos kilos desea llevar?"));
+        prompt("ingrese:" + parseInt(precio_producto) + $);
+        prompt("ingrese:" + (Kilos) + "Kgs");
+        alert(prompt(precio_total = precio_producto * Kilos));
+    }
+
+    if (opcion == "3") {
+        alert(prompt("Gracias por su compra"));
+    } else
+    break; */
+        
+
+
+
+    
+
+
+
+
+
+    
+
+
